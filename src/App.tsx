38593ae433b9589affe743bf23f943e2df26f692
Chris Wong <chriswonlihong@gmail.com>
2026-07-08 22:51:43 +0800

    Use full Syno logo in about section

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ import {
 import { useState } from 'react'
 import './App.css'
 
-const logoPath = '/assets/syno-mark.png'
+const logoPath = '/assets/syno-logo.png'
 const markPath = '/assets/syno-mark.png'
 
 const navItems = [
